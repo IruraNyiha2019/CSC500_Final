dc7d6d1ebb57846b427348d4d60309edff16b9f4
CREATEANDBUILD <33373263+createandbuild@users.noreply.github.com>
2019-05-21 12:26:32 -0400

    Update Mancala.java

--- Mancala.java
+++ Mancala.java
@@ -1,11 +1,50 @@
 
-public class Mancala
-{
+import java.util.Scanner;
 
-  public static void main(String[] args)
-  {
-    // TODO Auto-generated method stub
+public class Mancala {
+
+  public static void main(String[] args) {
+    int[] board = new int[14];
+
+    for (int k = 0; k < board.length; k++) {
+      if ((k + 1) % 7 == 0)
+        board[k] = 4;
+    }
+
+    startGame(board);
 
   }
+}
 
+class User {
+
+  private static int numSeeds;
+  private static int points;
+ 
+  static void startGame(int[] board){
+      Scanner in = new Scanner(System.in);
+  
+      System.out.println("Your move. Enter an integer between 0 and 5:");
+      int move = in.nextInt();
+      
+      int ogIndex = move;
+      numSeeds = board[move];
+      
+      for(int k = 0; k <= numSeeds; k++){
+        if(k + move == 14)
+          ogIndex = 0;
+        
+      }
+
+      for (int k = move + 1; k <= move + numSeeds; k++){
+          if (k == 14){
+            k=0;
+            
+          board[k]+=1;
+          }
+  
+        }
+  
+  }
+  
 }
